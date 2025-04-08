ce4776564df2485fc2fbb8c3d95b08ceddd7dbcd
Chirag9528 <chiragvarshney9528@gmail.com>
2025-04-08 21:25:24 +0530

    initial setup for frontend

---
+++ SwiftCart/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-5xl font-bold underline'>
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
